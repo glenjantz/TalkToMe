c702188276f8027f2956d74696a52d87807acbbd
Glen Jantz <glenjantz@Glens-MacBook-Pro.local>
2017-02-28 15:49:58 -0800

    more tests

--- apps/talktome/static/talktome/js/main.js
+++ apps/talktome/static/talktome/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var DIV = $('#thisdiv').get(0);
     DIV.scrollTop = DIV.scrollHeight;
     setInterval(function(){
-        $('#mydiv').load('/chatroom #thisdiv');
+        $('#thisdiv').load('/chatroom #thisdiv');
         console.log('hi');
     }, 1500);
 
