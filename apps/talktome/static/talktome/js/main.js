9450acb14c4a88c866a89acabc4c83b012d996c0
Glen Jantz <glenjantz@Glens-MacBook-Pro.local>
2017-02-28 13:16:47 -0800

    more tests

--- apps/talktome/static/talktome/js/main.js
+++ apps/talktome/static/talktome/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var DIV = $('#thisdiv').get(0);
     DIV.scrollTop = DIV.scrollHeight;
     setInterval(function(){
-        $('#mydiv').load('templates/talktome/chat.html' + '#thisdiv');
+        $('#mydiv').load('/chatroom #thisdiv');
         console.log('hi');
     }, 1500);
 
